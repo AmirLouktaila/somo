dace7579f649c5f87a1b48d20ac33d667fc5f85c
Salah Louktaila <86385687+AmirLouktaila@users.noreply.github.com>
2024-06-21 03:41:13 +0300

    se cart.js

--- cart.js
+++ cart.js
@@ -1,67 +1,50 @@
-const crypto = require("crypto");
 const axios = require('axios');
 
-class AliExpressLibraryCart {
-    constructor(AppKey, API_SECRET, Tracking_ID) {
-        this.API_URL = "https://api-sg.aliexpress.com/sync";
-        this.AppKey = AppKey;
-        this.API_SECRET = API_SECRET;
-        this.Tracking_ID = Tracking_ID;
-    }
+const portaffFunctioncart = async (cookie, id, trackingId) => {
+    const sourceTypes = ["", "620", "562", "561"];
 
-    hash(method, s, format) {
-        const sum = crypto.createHash(method);
-        const isBuffer = Buffer.isBuffer(s);
-        if (!isBuffer && typeof s === "object") {
-            s = JSON.stringify(this.sortObject(s));
-        }
-        sum.update(s, "utf8");
-        return sum.digest(format || "hex");
-    }
+    try {
+        const promotionLinkRequests = sourceTypes.map(sourceType => {
+            return axios.get("https://portals.aliexpress.com/tools/linkGenerate/generatePromotionLink.htm", {
+                params: {
+                    trackId: trackingId || 'default',
+                    targetUrl: id,
+                    sourceType: sourceType,
+                    aff_fcid: ''
+                },
+                headers: { "cookie": cookie }
+            });
+        });
 
-    sortObject(obj) {
-        return Object.keys(obj)
-            .sort()
-            .reduce(function (result, key) {
-                result[key] = obj[key];
-                return result;
-            }, {});
-    }
+        const infoRequest = axios.get(`https://alifetcher.onrender.com/info?id=${id}&lang=en_US`);
 
-    signRequest(parameters) {
-        const sortedParams = this.sortObject(parameters);
-        const sortedString = Object.keys(sortedParams).reduce((acc, objKey) => {
-            return `${acc}${objKey}${sortedParams[objKey]}`;
-        }, "");
-        const bookstandString = `${this.API_SECRET}${sortedString}${this.API_SECRET}`;
-        const signedString = this.hash("md5", bookstandString, "hex");
-        return signedString.toUpperCase();
-    }
+        const responses = await Promise.all([...promotionLinkRequests, infoRequest]);
 
-    async getData(link) {
-        const payload = {
-            app_key: this.AppKey,
-            sign_method: "md5",
-            timestamp: new Date().toISOString().slice(0, 19).replace('T', ' '),
-            format: "json",
-            v: "2.0",
-            method: "aliexpress.affiliate.link.generate",
-            promotion_link_type: 0,
-            tracking_id: this.Tracking_ID,
-            source_values: link,
-        };
-        const sign = this.signRequest(payload);
-        const allParams = {
-            ...payload,
-            sign,
-        };
-        try {
-            const response = await axios.post(this.API_URL, new URLSearchParams(allParams));
-            return response.data.aliexpress_affiliate_link_generate_response.resp_result.result.promotion_links.promotion_link[0].promotion_link;
-        } catch (error) {
-            throw new Error(`API Request Error: ${error.message}`);
-        }
-    }
-}
+        let result = responses[4].data; // Accessing data from infoRequest
+        console.log(result);
 
-module.exports = AliExpressLibraryCart;
+        result.aff = {};
+
+        responses.slice(0, 3).forEach((response, index) => {
+            switch (index) {
+                case 0: // Points
+                    result.aff.points = response.data.data;
+                    break;
+                case 1: // Super
+                    result.aff.super = response.data.data;
+                    break;
+                case 2: // Limited
+                    result.aff.limited = response.data.data;
+                    break;
+            }
+        });
+
+        return result;
+    } catch (error) {
+        console.log("portaffFunction ERR : ", error.code);
+        return "ERR";
+    }
+};
+
+// Example usage:
+exports.portaffFunctioncart = portaffFunctioncart;
