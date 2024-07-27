5f79f2dc54fc9308c32600b1b3835bfb3753b524
Salah Louktaila <86385687+AmirLouktaila@users.noreply.github.com>
2024-07-28 02:41:22 +0300

    fate afflinker.js

--- afflinker.js
+++ afflinker.js
@@ -14,7 +14,7 @@ const portaffFunction = async (cookie, id, trackingId) => {
       });
     });
 
-    const infoRequest = axios.get(`https://notibest.notibyte-dz.com/info?id=${id}&lang=en_US`);
+    const infoRequest = axios.get(`https://notibest.notibyte-dz.com/info?id=${id}`);
 
     const responses = await Promise.all([...promotionLinkRequests, infoRequest]);
 
